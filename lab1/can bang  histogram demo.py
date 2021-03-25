d62c01365e4d2f6b12587bae3c2e5eaf4bd63ab2
QuangG23 <nnquang.code@gmail.com>
2021-03-25 22:33:05 +0700

    vnai files

--- lab1/can bang  histogram demo.py
+++ lab1/can bang  histogram demo.py
@@ -17,6 +17,22 @@ plt.close()
 
 
 
+# from __future__ import print_function
+# import cv2 as cv
+# import argparse
+# parser = argparse.ArgumentParser(description='Code for Histogram Equalization tutorial.')
+# parser.add_argument('--input', help='Path to input image.', default='rudi.jpg')
+# args = parser.parse_args()
+# src = cv.imread(cv.samples.findFile(args.input))
+# if src is None:
+#     print('Could not open or find the image:', args.input)
+#     exit(0)
+# src = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+# dst = cv.equalizeHist(src)
+# cv.imshow('Source image', src)
+# cv.imshow('Equalized Image', dst)
+# cv.waitKey()
+
 # import numpy as np
 # import scipy.misc, math
 # from scipy.misc.pilutil import Image
